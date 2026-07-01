b7eff051c8a630827475821c9ebfe72f7307c8e8
stavmo <stavmo@post.bgu.ac.il>
2026-07-01 20:35:27 +0300

    added 3 and theorical questions + final zip

--- ex4.pl
+++ ex4.pl
@@ -18,28 +18,42 @@ maximum_printing_depth(100).
 % Purpose: All elements in Sublist appear in List in the same order.
 % Precondition: List is fully instantiated (queries do not include variables in their second argument).
 
-% not so sure about this implementation, i will test this later
-sub_list([], List)
-sub_list([X | X1], [X | X2]) :- sub_list(X1, X2)
-sub_list(X1, [Y | X2]) :- sub_list(X1, X2)
+sub_list([], _).
+sub_list([X|Xs], [X|Ys]) :- sub_list(Xs, Ys).
+sub_list([X|Xs], [_|Ys]) :- sub_list([X|Xs], Ys).
 
 
 
 % Signature: swap_list(List, InversedList)/2
 % Purpose: InversedList is the ‘mirror’ representation of List, i.e, each item in the list is recursively replaced with the item at the position, with refers to the beginning and the end of the list.   
 
+swap_list([H|T], InversedList) :-
+    !,
+    swap_list(H, RevH),
+    swap_list(T, RevT),
+    app(RevT, [RevH], InversedList).
+swap_list(X, X).
 
-
-
-
-
+% Helper for swap_list: An implementation of append/3.
+% Signature: app(List1, List2, Result)/3
+% Purpose: Concatenates List1 and List2 into Result
+app([], L, L).
+app([H|T], L, [H|R]) :- app(T, L, R).
 
 
 % Signature: sub_tree(Subtree, Tree)/2
 % Purpose: Tree contains Subtree.
 
+sub_tree(T, T).
+sub_tree(Sub, tree(_, Left, _)) :- sub_tree(Sub, Left).
+sub_tree(Sub, tree(_, _, Right)) :- sub_tree(Sub, Right).
 
 
 
 % Signature: swap_tree(Tree, InversedTree)/2
 % Purpose: InversedTree is the �mirror� representation of Tree.
+
+swap_tree(void, void).
+swap_tree(tree(Root, Left, Right), tree(Root, InvRight, InvLeft)) :-
+    swap_tree(Left, InvLeft),
+    swap_tree(Right, InvRight).
